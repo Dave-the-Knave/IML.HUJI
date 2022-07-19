2bb4d7bc531ba616f560b127a2e14f01c6f70804
Dave-the-Knave <101048801+Dave-the-Knave@users.noreply.github.com>
2022-07-19 16:23:15 +0300

    Add files via upload

--- IMLearn/model_selection/cross_validate.py
+++ IMLearn/model_selection/cross_validate.py
@@ -1,12 +1,12 @@
 from __future__ import annotations
 from copy import deepcopy
-from typing import Tuple, Callable
+from typing import Tuple, Callable, Type
 import numpy as np
 from IMLearn import BaseEstimator
 
 
 def cross_validate(estimator: BaseEstimator, X: np.ndarray, y: np.ndarray,
-                   scoring: Callable[[np.ndarray, np.ndarray, ...], float], cv: int = 5) -> Tuple[float, float]:
+                   scoring: Callable[[np.ndarray, np.ndarray, ...], float], cv: int = 5) -> tuple[float, float]:
     """
     Evaluate metric by cross-validation for given estimator
 
@@ -37,4 +37,23 @@ def cross_validate(estimator: BaseEstimator, X: np.ndarray, y: np.ndarray,
     validation_score: float
         Average validation score over folds
     """
-    raise NotImplementedError()
+    # create list of indexes corresponding to indexes of X/y, labeled with integers in range cv
+    # iterate through cv
+    # load an array with indexes except for those labeled cv and a corresponding series
+    # fit the estimator, predict and store the score
+    # end iteration and average the scores
+    m = X.shape[0]
+    indices = np.arange(0, m)
+    t_score = []
+    v_score = []
+    for i in range(cv):
+        validation_indices = indices % cv == i
+        k_minus_one_indices = indices % cv != i
+        train = X[k_minus_one_indices]
+        train_labels = y[k_minus_one_indices]
+        validation = X[validation_indices]
+        validation_labels = y[validation_indices]
+        estimator.fit(train, train_labels)
+        t_score.append(scoring(estimator.predict(train), train_labels))
+        v_score.append(scoring(estimator.predict(validation), validation_labels))
+    return sum(t_score)/len(t_score), sum(v_score)/len(v_score)
